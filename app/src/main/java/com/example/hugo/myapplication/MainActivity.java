55b46f15298e245c8c558014ed13a076895db697
hugomatheuss <hugomatheus650@gmail.com>
2017-08-18 10:16:54 -0300

    commit 2

--- app/src/main/java/com/example/hugo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hugo/myapplication/MainActivity.java
@@ -13,51 +13,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 1;
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void submitOrder(View v) {
         String message = "Total: " + quantity;
-        message = message + "\nValor: R$" + quantity*5;
+        int price = calculatePrice(quantity);
+        message = message + "\nValor: R$" + price;
         message = message + "\nObrigado!";
+
+
         displayMessage(message);
     }
 
@@ -76,16 +47,16 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_textview);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+
 
     private void displayMessage(String message) {
         TextView messageTextView = (TextView) findViewById(R.id.price_textview);
         messageTextView.setText(message);
+    }
+
+
+    private int calculatePrice(int quantity) {
+        int price = quantity * 5;
+        return price;
     }
 }
